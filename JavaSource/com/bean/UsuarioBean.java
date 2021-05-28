34081aa2f7101d66627c2b10668f0cf7979f4fe8
Santiago Da Silva <sdasilva@outlook.es>
2021-05-28 19:17:53 -0300

    Usuario Bean alta y modificacion

--- JavaSource/com/bean/UsuarioBean.java
+++ JavaSource/com/bean/UsuarioBean.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import com.entidad.Rol;
+import com.entidad.Roles;
 import com.entidad.Usuario;
 import com.remote.UsuarioCliente;
 
@@ -19,18 +21,30 @@ public class UsuarioBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private String nombre;
+	private String apellido;
+	private String documento;
+	private String clave;
+	private String email;
+	private String nickname;
+	private String instituto;
+	private String profesion;
+	private String rol;
+	
 	private String selectedNombre;
 	private String selectedRol;
 	
 	private List<Usuario> usuarios;
 	private List<Usuario> selectedUsuarios;
 	private Usuario selectedUsuario;
+	private Usuario createUsuario;
 	
 	UsuarioCliente usuario_;
 
 	public UsuarioBean() {
 		usuario_ = new UsuarioCliente();
 		this.usuarios = usuario_.getBean().readAll();
+		this.selectedUsuarios = this.usuarios;
 		System.out.println("Construyo el Bean de Usuarios");
 	}
 	
@@ -74,6 +88,85 @@ public class UsuarioBean implements Serializable {
 		System.out.println("Se ha selecionado el usuario con id: " + selectedUsuario.getId());
 		this.selectedUsuario = selectedUsuario;
 	}
+	
+	/**
+	 * 
+	 * Para dar alta
+	 */
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getInstituto() {
+		return instituto;
+	}
+
+	public void setInstituto(String instituto) {
+		this.instituto = instituto;
+	}
+
+	public String getProfesion() {
+		return profesion;
+	}
+
+	public void setProfesion(String profesion) {
+		this.profesion = profesion;
+	}
+	
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	
+	// FIN
 
 	public String filterUsuarios() {
 		this.usuarios = usuario_.getBean().readAll();
@@ -98,8 +191,46 @@ public class UsuarioBean implements Serializable {
 		return "";
 	}
 	
-	public String modificar() {
-		return "altaUsuario";
-	}
+	
 
+	public String modificar() {
+		return "modificarUsuario";
+	}
+	
+	public String modificarUsuario() {
+		usuario_.getBean().update(selectedUsuario);
+		this.usuarios = usuario_.getBean().readAll();
+		this.selectedUsuarios = this.usuarios;
+		return "usuarios";
+	}
+	
+	public String crearUsuario() {
+		createUsuario = new Usuario();
+		createUsuario.setNombre(nombre);
+		createUsuario.setApellido(apellido);
+		createUsuario.setDocumento(documento);
+		createUsuario.setClave(clave);
+		createUsuario.setEmail(email);
+		createUsuario.setNickname(nickname);
+		createUsuario.setInstituto(instituto);
+		createUsuario.setProfesion(profesion);
+		
+		switch(rol.toUpperCase()) {
+		case "COMUN":
+			createUsuario.setRol(Roles.COMUN);
+			break;
+		case "EXPERTO":
+			createUsuario.setRol(Roles.EXPERTO);
+			break;
+		case "ADMINISTRADOR":
+			createUsuario.setRol(Roles.ADMINISTRADOR);
+			break;
+		}
+		
+		usuario_.getBean().create(createUsuario);
+		this.usuarios = usuario_.getBean().readAll();
+		this.selectedUsuarios = this.usuarios;
+		return "usuarios";
+	}
+		
 }
